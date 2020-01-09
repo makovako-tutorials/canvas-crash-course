a024064f16ed8efa95481f928409a4c10794986a
Marek Valko <valko.marek@gmail.com>
2020-01-09 15:06:43 +0100

    Separeted into canvases

--- script.js
+++ script.js
@@ -1,100 +1,106 @@
-const canvas = document.getElementById('canvas')
+const canvas = [
+    document.getElementById('canvas1'),
+    document.getElementById('canvas2'),
+    document.getElementById('canvas3'),
+    document.getElementById('canvas4'),
+    document.getElementById('canvas5')
+]
 
 // Drawing done using context
 
-const ctx = canvas.getContext('2d')
+const ctx = canvas.map(canvas => canvas.getContext('2d'))
 
-if (false) {
+if (true) {
 
     // canvas.width = window.innerWidth
     
     //fillRect
     
-    ctx.fillStyle = 'red'
-    ctx.fillRect(20,20,150,100)
-    ctx.fillRect(200,20,150,100)
+    ctx[0].fillStyle = 'red'
+    ctx[0].fillRect(20,20,150,100)
+    ctx[0].fillRect(200,20,150,100)
     
     // strokeREct
     
-    ctx.strokeStyle = 'green'
-    ctx.lineWidth = 5
-    ctx.strokeRect(100,200,150,100)
+    ctx[0].strokeStyle = 'green'
+    ctx[0].lineWidth = 5
+    ctx[0].strokeRect(100,200,150,100)
     
     // clearRect - clears part of rect (for animations)
     
-    ctx.clearRect(25,25,140,90)
+    ctx[0].clearRect(25,25,140,90)
     
     //fillText
     
-    ctx.fillStyle = 'blue'
-    ctx.font = "30px Arial"
-    ctx.fillText("Hello world",500,50)
+    ctx[0].fillStyle = 'blue'
+    ctx[0].font = "30px Arial"
+    ctx[0].fillText("Hello world",500,50)
     
     // strokeText
     
-    ctx.strokeStyle = 'orange'
-    ctx.lineWidth = 2
-    ctx.strokeText("Hello world", 500, 70)
+    ctx[0].strokeStyle = 'orange'
+    ctx[0].lineWidth = 2
+    ctx[0].strokeText("Hello world", 500, 70)
 }
 
-if (false) {
+if (true) {
     //paths
-    ctx.beginPath();
-    ctx.moveTo(50,50)
-    ctx.lineTo(150,50)
-    ctx.lineTo(100,200)
-    // ctx.lineTo(50,50)
-    ctx.closePath()
-    // ctx.stroke()
-    ctx.fillStyle = 'coral'
-    ctx.fill()
+    ctx[1].beginPath();
+    ctx[1].moveTo(50,50)
+    ctx[1].lineTo(150,50)
+    ctx[1].lineTo(100,200)
+    // ctx[1].lineTo(50,50)
+    ctx[1].closePath()
+    // ctx[1].stroke()
+    ctx[1].fillStyle = 'coral'
+    ctx[1].fill()
 
-    ctx.beginPath()
-    ctx.moveTo(200,50)
-    ctx.lineTo(150,200)
-    ctx.lineTo(250,200)
-    ctx.closePath()
-    ctx.stroke()
+    ctx[1].beginPath()
+    ctx[1].moveTo(200,50)
+    ctx[1].lineTo(150,200)
+    ctx[1].lineTo(250,200)
+    ctx[1].closePath()
+    ctx[1].stroke()
 
-    ctx.beginPath()
-    ctx.rect(300,50, 150, 100)
-    ctx.fill()
+    ctx[1].beginPath()
+    ctx[1].rect(300,50, 150, 100)
+    ctx[1].fill()
     
 }
 
-if (false) {
+if (true) {
 
-    const centerX = canvas.width / 2
-    const centerY = canvas.height / 2
+    const centerX = canvas[2].width / 2
+    const centerY = canvas[2].height / 2
     //Arcs
-    ctx.beginPath()
+    ctx[2].beginPath()
 
     // head
-    ctx.arc(centerX,centerY , 200, 0, Math.PI * 2)
+    ctx[2].arc(centerX,centerY , 200, 0, Math.PI * 2)
 
     // move to mouth
-    ctx.moveTo(centerX + 100, centerY)
+    ctx[2].moveTo(centerX + 100, centerY)
 
     // draw mount
-    ctx.arc(centerX, centerY, 100, 0, Math.PI, false)
+    ctx[2].arc(centerX, centerY, 100, 0, Math.PI, false)
 
-    // ctx.closePath()
+    // ctx[2].closePath()
 
     //move to left eye
 
-    ctx.moveTo(centerX-60, centerY-80)
+    ctx[2].moveTo(centerX-60, centerY-80)
 
     //draw left eye
-    ctx.arc(centerX-80, centerY-80, 20, 0, Math.PI * 2)
+    ctx[2].arc(centerX-80, centerY-80, 20, 0, Math.PI * 2)
 
 
     //move to right eye
 
-    ctx.moveTo(centerX+100, centerY-80)
+    ctx[2].moveTo(centerX+100, centerY-80)
 
     //draw right eye
-    ctx.arc(centerX+80, centerY-80, 20, 0, Math.PI * 2)
-    ctx.stroke()
+    ctx[2].arc(centerX+80, centerY-80, 20, 0, Math.PI * 2)
+    ctx[2].stroke()
 }
 
 if (false) {
@@ -105,7 +111,7 @@ if (false) {
 
 
 // animation 1
-if (false) {
+if (true) {
     const circle = {
         x: 200,
         y: 200,
@@ -115,14 +121,14 @@ if (false) {
     }
 
     const drawCircle = () => {
-        ctx.beginPath()
-        ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2)
-        ctx.fillStyle = 'purple'
-        ctx.fill()
+        ctx[3].beginPath()
+        ctx[3].arc(circle.x, circle.y, circle.size, 0, Math.PI * 2)
+        ctx[3].fillStyle = 'purple'
+        ctx[3].fill()
     }
 
     const update = () => {
-        ctx.clearRect(0,0,canvas.width, canvas.height)
+        ctx[3].clearRect(0,0,canvas[3].width, canvas[3].height)
 
         drawCircle()
 
@@ -132,10 +138,10 @@ if (false) {
 
         // detect side walls
 
-        if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
+        if (circle.x + circle.size > canvas[3].width || circle.x - circle.size < 0) {
             circle.dx *= -1
         }
-        if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0) {
+        if (circle.y + circle.size > canvas[3].height || circle.y - circle.size < 0) {
             circle.dy*= -1
         }
 
@@ -162,11 +168,11 @@ if (true) {
     }
 
     const drawPlayer = () => {
-        ctx.drawImage(image, player.x, player.y, player.w, player.h)
+        ctx[4].drawImage(image, player.x, player.y, player.w, player.h)
     }
 
     const clear = () => {
-        ctx.clearRect(0,0, canvas.width, canvas.height)
+        ctx[4].clearRect(0,0, canvas[4].width, canvas[4].height)
     }
 
     const detectWalls = () => {
@@ -176,11 +182,11 @@ if (true) {
         else if (player.y < 0) {
             player.y = 0
         }
-        else if (player.x + player.w > canvas.width) {
-            player.x = canvas.width - player.w
+        else if (player.x + player.w > canvas[4].width) {
+            player.x = canvas[4].width - player.w
         }
-        else if (player.y + player.h > canvas.height) {
-            player.y = canvas.height - player.h
+        else if (player.y + player.h > canvas[4].height) {
+            player.y = canvas[4].height - player.h
         }
         
     }
@@ -220,6 +226,7 @@ if (true) {
     }
 
     const keyDown = (e) => {
+        e.preventDefault()
         if (e.key === 'ArrowRight' || e.key === 'Right') {
             moveRight()
         }
